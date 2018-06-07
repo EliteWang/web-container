c45f125346dcb0c55358651eaaf854190fdb8e97
EliteWang <wyn945628908@gmail.com>
2018-06-07 15:40:06 +0800

    web container

---
+++ web-container/src/main/java/com/yanan/webcontainer/WebContainerApplication.java
@@ -0,0 +1,12 @@
+package com.yanan.webcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebContainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebContainerApplication.class, args);
+    }
+}
